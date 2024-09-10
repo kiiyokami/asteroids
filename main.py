6368c8add16655f1c43888a4710e19898168b958
kiiyokami <hiimkaze@gmail.com>
2024-09-10 16:01:30 +0800

    update

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
+        # Clear the screen
+        screen.fill((0, 0, 0))
+
+        # Draw asteroids
+
+        # Update the screen
+        pygame.display.flip()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return False
   
 
 
